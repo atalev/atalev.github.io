1297ce5c42956861394471738b245fafcdf3dbb1
Anass Taleb <ataleb@enssat.fr>
2021-02-17 00:21:59 +0100

    Finalised WebWorkers
    Added POO implementation and multiple webworkers(6)

--- CircleWorker.js
+++ CircleWorker.js
@@ -1,5 +1,8 @@
 onmessage = function(e) {
     var intervalID;
+    var radius = e.data[0];
+    var color = e.data[1];
+
 
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
@@ -10,7 +13,6 @@ onmessage = function(e) {
     function generateCoord(){
         x=getRandomInt(600);
         y=getRandomInt(600);
-        postMessage([x,y]);
+        postMessage([x,y,radius,color]);
     }
-
-  }
+}
